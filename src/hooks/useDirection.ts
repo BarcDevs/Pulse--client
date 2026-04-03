0d4b96b33373434a7ef5df453a00d74ab990a1d8
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2026-04-03 18:26:59 +0300

    fix: accessing document before full client loads

--- src/hooks/useDirection.ts
+++ src/hooks/useDirection.ts
@@ -1,5 +1,19 @@
 'use client'
 
-export const useDirection = () =>
-    (document.documentElement.dir as 'ltr' | 'rtl') ||
-    'ltr'
+import {
+    useEffect,
+    useState
+} from 'react'
+
+export const useDirection = () => {
+    const [dir, setDir] = useState<'ltr' | 'rtl'>('ltr')
+
+    useEffect(() => {
+        setDir(
+            (document?.documentElement?.dir as 'ltr' | 'rtl') ||
+            'ltr'
+        )
+    }, [])
+
+    return dir
+}
