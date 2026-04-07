0d2cca6dbe3b6d8caaffc5be6617b27ef32ec7b0
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2026-04-07 20:09:19 +0300

    rfc: remove unused Metric, Page and UserLabel components

--- src/components/shared/ui/Page.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from 'react'
-
-export const Page = ({ children }: PropsWithChildren) => (
-    <main className={'min-h-screen w-full p-6'}>
-        {children}
-    </main>
-)
